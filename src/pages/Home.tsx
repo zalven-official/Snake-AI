491a11a29132607e0aea980fedfe4715cb86605e
zalven-official <zalvendayao888@gmail.com>
2022-10-07 23:16:40 +0800

    finishing snake AI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getRandomInt } from '../helpers/randomizer';
 import { distanceBetweenTwoPoints } from '../helpers/colliders';
 import useFrameLoop from '../helpers/useFrameLoop';
@@ -6,23 +6,17 @@ import useFrameLoop from '../helpers/useFrameLoop';
 const width = 58;
 const height = 38;
 
-function isMoveAllowed(snakeMove: string, movement: string) {
-  if (snakeMove === 'D') return movement !== 'U';
-  if (snakeMove === 'U') return movement !== 'D';
-  if (snakeMove === 'R') return movement !== 'L';
-  if (snakeMove === 'L') return movement !== 'R';
-  return false;
-}
-
 function Home() {
+  const [createBarrier, setCreateBarrier] = useState(false);
+  // Game mode
+  const [isDead, setIsDead] = useState(false);
   // Snake Properties
   const [snakeBody, setSnakeBody] = useState<Array<Array<number>>>([
     [width / 2, height / 2 - 2],
     [width / 2, height / 2 - 1],
     [width / 2, height / 2],
   ]);
-  // Snake Movement (U: Up, D: Down, L: Left, R: Right)
-  const [snakeMove, setSnakeMove] = useState<string>('D');
+  const [score, setScore] = useState(0);
 
   // Snake Food
   const [snakeFood, setSnakeFood] = useState<Array<number>>([
@@ -34,12 +28,200 @@ function Home() {
   // Snake path
   const [snakePath, setSnakePath] = useState<Array<Array<number>>>();
 
-  // Change movement of snake
-  function changeMovement(movement: string) {
-    if (isMoveAllowed(snakeMove, movement)) setSnakeMove(movement);
+  // A* Algorithm
+  function pathFinder() {
+    if (isDead) return '';
+    const start = snakeBody[snakeBody.length - 1];
+    const paths: Array<Array<number>> = [start];
+    const allPath: Array<Array<number>> = [start];
+    // Create env
+    const env = [...Array(width)].map(() => Array(height));
+    const envPath = [...Array(width)].map(() => Array(height));
+    envPath[start[0]][start[1]] = 1;
+    let foodFind = false;
+
+    // Env Patterns for O(N) search
+    const notAllowedPattern = -1;
+
+    for (let i = 0; i < snakeBody.length; i += 1) {
+      env[snakeBody[i][0]][snakeBody[i][1]] = notAllowedPattern;
+    }
+    while (paths.length > 0) {
+      const x = paths[0][0];
+      const y = paths[0][1];
+      const cost = envPath[x][y];
+
+      const pathDirection: Array<Array<number>> = [];
+
+      // Up
+      if (y > 0 && env[x][y - 1] !== notAllowedPattern) {
+        const isUp = [x, y - 1];
+        allPath.push(isUp);
+        pathDirection.push(isUp);
+        env[x][y - 1] = notAllowedPattern;
+        envPath[x][y - 1] = cost + 1;
+        if (x === snakeFood[0] && y - 1 === snakeFood[1]) {
+          foodFind = true;
+          break;
+        }
+      }
+
+      //   Down
+      if (y < height - 1 && env[x][y + 1] !== notAllowedPattern) {
+        const isDown = [x, y + 1];
+        allPath.push(isDown);
+        pathDirection.push(isDown);
+        env[x][y + 1] = notAllowedPattern;
+        envPath[x][y + 1] = cost + 1;
+        if (x === snakeFood[0] && y + 1 === snakeFood[1]) {
+          foodFind = true;
+          break;
+        }
+      }
+
+      //  Left
+      if (x > 0 && env[x - 1][y] !== notAllowedPattern) {
+        const isLeft = [x - 1, y];
+        allPath.push(isLeft);
+        pathDirection.push(isLeft);
+        env[x - 1][y] = notAllowedPattern;
+        envPath[x - 1][y] = cost + 1;
+        if (x - 1 === snakeFood[0] && y === snakeFood[1]) {
+          foodFind = true;
+          break;
+        }
+      }
+
+      //  Right
+      if (x < width - 1 && env[x + 1][y] !== notAllowedPattern) {
+        const isRight = [x + 1, y];
+        allPath.push(isRight);
+        pathDirection.push(isRight);
+        env[x + 1][y] = notAllowedPattern;
+        envPath[x + 1][y] = cost + 1;
+        if (x + 1 === snakeFood[0] && y === snakeFood[1]) {
+          foodFind = true;
+          break;
+        }
+      }
+      pathDirection.sort((a, b) => {
+        const aDist = distanceBetweenTwoPoints(
+          a[0],
+          a[1],
+          snakeFood[0],
+          snakeFood[1]
+        );
+        const bDist = distanceBetweenTwoPoints(
+          b[0],
+          b[1],
+          snakeFood[0],
+          snakeFood[1]
+        );
+        return bDist - aDist;
+      });
+      // A* Algorithm
+      for (let i = 1; i < paths.length; i += 1) {
+        const distPath = distanceBetweenTwoPoints(
+          paths[i][0],
+          paths[i][1],
+          snakeFood[0],
+          snakeFood[1]
+        );
+        if (pathDirection.length >= 1) {
+          const distDirection = distanceBetweenTwoPoints(
+            pathDirection[0][0],
+            pathDirection[0][1],
+            snakeFood[0],
+            snakeFood[1]
+          );
+          if (distDirection <= distPath) {
+            const direction = pathDirection.shift();
+            if (direction) {
+              paths.splice(i, 0, direction);
+              i = 0;
+            }
+          }
+        }
+      }
+      for (let i = 0; i < pathDirection.length; i += 1)
+        paths.push(pathDirection[i]);
+
+      paths.shift();
+
+      setSnakePathFind(allPath);
+
+      if (x === snakeFood[0] && y === snakeFood[1]) {
+        foodFind = true;
+        break;
+      }
+    }
+    if (foodFind) setScore(score + 1);
+
+    // Back track
+    if (allPath.length >= 1 && foodFind) {
+      const backtrackPath: Array<Array<number>> = [];
+      let findEnd: Array<number> = [...snakeFood];
+      for (
+        let i = 0;
+        i < allPath.length &&
+        findEnd.length >= 1 &&
+        envPath[findEnd[0]][findEnd[1]] > 1;
+        i += 1
+      ) {
+        const x = findEnd[0];
+        const y = findEnd[1];
+
+        const pathDirection: Array<Array<number>> = [];
+        // Up
+        if (y > 0 && envPath[x][y - 1] < envPath[x][y]) {
+          const isUp = [x, y - 1, 1];
+          if (env[x][y - 1] === notAllowedPattern) pathDirection.push(isUp);
+        }
+        //   Down
+        if (y < height - 1 && envPath[x][y + 1] < envPath[x][y]) {
+          const isDown = [x, y + 1, 2];
+          if (env[x][y + 1] === notAllowedPattern) pathDirection.push(isDown);
+        }
+        //  Left
+        if (x > 0 && envPath[x - 1][y] < envPath[x][y]) {
+          const isLeft = [x - 1, y, 3];
+          if (env[x - 1][y] === notAllowedPattern) pathDirection.push(isLeft);
+        }
+        //  Right
+        if (x < width - 1 && envPath[x + 1][y] < envPath[x][y]) {
+          const isRight = [x + 1, y, 4];
+          if (env[x + 1][y] === notAllowedPattern) pathDirection.push(isRight);
+        }
+        pathDirection.sort((a, b) => {
+          const aDist = envPath[a[0]][a[1]];
+          const bDist = envPath[b[0]][b[1]];
+          return aDist - bDist;
+        });
+        if (pathDirection.length >= 1) {
+          findEnd = [...pathDirection[0]];
+          backtrackPath.push(pathDirection[0]);
+          const z = [];
+          for (let j = 0; j < pathDirection.length; j += 1) {
+            z.push(envPath[pathDirection[j][0]][pathDirection[j][1]]);
+          }
+        } else {
+          break;
+        }
+      }
+      setSnakePath(backtrackPath);
+
+      const lastInd = backtrackPath.length - 1;
+      if (backtrackPath[lastInd][2] === 1) return 'D';
+      if (backtrackPath[lastInd][2] === 2) return 'U';
+      if (backtrackPath[lastInd][2] === 3) return 'R';
+      if (backtrackPath[lastInd][2] === 4) return 'L';
+    }
+
+    return '';
   }
+
   // Movement
-  function moveSnakeBody() {
+  function moveSnakeBody(snakeMove: string) {
     const snakeLength = snakeBody.length;
     const body = [...snakeBody];
     let move: Array<number> = [];
@@ -59,254 +241,93 @@ function Home() {
 
     // Death
     if (move[0] < 0 || move[0] >= width || move[1] < 0 || move[1] >= height)
-      console.log(move);
-
-    // Change food and eat food
-    const eatFood = move[0] === snakeFood[0] && move[1] === snakeFood[1];
-    if (eatFood) {
-      for (let i = 0; i < 2000; i += 1) {
-        const dropX = getRandomInt(0, width - 1);
-        const dropY = getRandomInt(0, height - 1);
-        let hasDroppedAvailable = true;
-        for (let j = 0; j < body.length; j += 1) {
-          const bodyDropX = body[j][0];
-          const bodyDropY = body[j][1];
-          if (dropX === bodyDropX && dropY === bodyDropY) {
-            hasDroppedAvailable = false;
+      setIsDead(true);
+    for (let i = 0; i < body.length; i += 1) {
+      if (body[i][0] === move[0] && body[i][1] === move[1]) {
+        setIsDead(true);
+        break;
+      }
+    }
+    if (!isDead) {
+      // Change food and eat food
+      const eatFood = move[0] === snakeFood[0] && move[1] === snakeFood[1];
+      if (eatFood) {
+        for (let i = 0; i < 2000; i += 1) {
+          const dropX = getRandomInt(0, width - 1);
+          const dropY = getRandomInt(0, height - 1);
+          let hasDroppedAvailable = true;
+          for (let j = 0; j < body.length; j += 1) {
+            const bodyDropX = body[j][0];
+            const bodyDropY = body[j][1];
+            if (dropX === bodyDropX && dropY === bodyDropY) {
+              hasDroppedAvailable = false;
+              break;
+            }
+          }
+          if (hasDroppedAvailable) {
+            setSnakeFood([dropX, dropY]);
             break;
           }
         }
-        if (hasDroppedAvailable) {
-          setSnakeFood([dropX, dropY]);
-          break;
-        }
       }
+      if (move.length === 2) body.push(move);
+      if (snakeLength > 1 && !eatFood) body.shift();
+      setSnakeBody(body);
     }
-
-    // Move Head
-    if (move.length === 2) body.push(move);
-    // Remove Tail
-    if (snakeLength > 1 && !eatFood) body.shift();
-
-    setSnakeBody(body);
   }
 
-  useEffect(() => {
-    const start = snakeBody[snakeBody.length - 1];
-    const paths: Array<Array<number>> = [start];
-    const allPath: Array<Array<number>> = [start];
-    // Create env
-    const env = [...Array(width)].map(() => Array(height));
-
-    // Env Patterns for O(N) search
-    const notAllowedPattern = -1;
-
-    for (let i = 0; i < snakeBody.length; i += 1) {
-      env[snakeBody[i][0]][snakeBody[i][1]] = notAllowedPattern;
+  function handleCheckChange() {
+    if (createBarrier) {
+      setSnakeBody([
+        [width / 2, height / 2 - 2],
+        [width / 2, height / 2 - 1],
+        [width / 2, height / 2],
+      ]);
     }
-    while (paths.length > 0) {
-      const x = paths[0][0];
-      const y = paths[0][1];
+    setCreateBarrier(!createBarrier);
+  }
 
-      const pathDirection: Array<Array<number>> = [];
-
-      // Up
-      if (y > 0 && env[x][y - 1] !== notAllowedPattern) {
-        const isUp = [x, y - 1];
-        allPath.push(isUp);
-        pathDirection.push(isUp);
-        env[x][y - 1] = notAllowedPattern;
-        if (x === snakeFood[0] && y - 1 === snakeFood[1]) break;
-      }
-
-      //   Down
-      if (y < height - 1 && env[x][y + 1] !== notAllowedPattern) {
-        const isDown = [x, y + 1];
-        allPath.push(isDown);
-        pathDirection.push(isDown);
-        env[x][y + 1] = notAllowedPattern;
-        if (x === snakeFood[0] && y + 1 === snakeFood[1]) break;
-      }
-
-      //  Left
-      if (x > 0 && env[x - 1][y] !== notAllowedPattern) {
-        const isLeft = [x - 1, y];
-        allPath.push(isLeft);
-        pathDirection.push(isLeft);
-        env[x - 1][y] = notAllowedPattern;
-        if (x - 1 === snakeFood[0] && y === snakeFood[1]) break;
-      }
-
-      //  Right
-      if (x < width - 1 && env[x + 1][y] !== notAllowedPattern) {
-        const isRight = [x + 1, y];
-        allPath.push(isRight);
-        pathDirection.push(isRight);
-        env[x + 1][y] = notAllowedPattern;
-        if (x + 1 === snakeFood[0] && y === snakeFood[1]) break;
-      }
-      pathDirection.sort((a, b) => {
-        const aDist = distanceBetweenTwoPoints(
-          a[0],
-          a[1],
-          snakeFood[0],
-          snakeFood[1]
-        );
-        const bDist = distanceBetweenTwoPoints(
-          b[0],
-          b[1],
-          snakeFood[0],
-          snakeFood[1]
-        );
-        return bDist - aDist;
-      });
-
-      for (let i = 1; i < paths.length; i += 1) {
-        const distPath = distanceBetweenTwoPoints(
-          paths[i][0],
-          paths[i][1],
-          snakeFood[0],
-          snakeFood[1]
-        );
-        const direction = pathDirection.shift();
-        if (direction !== undefined) {
-          const distDirection = distanceBetweenTwoPoints(
-            direction[0],
-            direction[1],
-            snakeFood[0],
-            snakeFood[1]
-          );
-          if (distDirection <= distPath) {
-            paths.splice(i, 0, direction);
-            i = 0;
-          }
-        }
-      }
-      for (let i = 0; i < pathDirection.length; i += 1)
-        paths.push(pathDirection[i]);
-      paths.shift();
-      setSnakePathFind(allPath);
-
-      if (x === snakeFood[0] && y === snakeFood[1]) break;
-      // Back track
+  function addBarrier(x: number, y: number) {
+    if (createBarrier) {
+      const result = [...snakeBody];
+      result.unshift([x, y]);
+      setSnakeBody(result);
     }
-    allPath.sort((a, b) => {
-      const aDist = distanceBetweenTwoPoints(
-        a[0],
-        a[1],
-        snakeFood[0],
-        snakeFood[1]
-      );
-      const bDist = distanceBetweenTwoPoints(
-        b[0],
-        b[1],
-        snakeFood[0],
-        snakeFood[1]
-      );
-      return bDist - aDist;
-    });
-    if (allPath.length >= 1) {
-      // Create a reset env for backtracking
-      const envBacktrack = [...Array(width)].map(() => Array(height));
-      for (let i = 0; i < snakeBody.length; i += 1) {
-        envBacktrack[snakeBody[i][0]][snakeBody[i][1]] = notAllowedPattern;
-      }
-      let findEnd: Array<number> = [...start];
-      const backtrackPath: Array<Array<number>> = [];
-      while (findEnd[0] !== snakeFood[0] || findEnd[1] !== snakeFood[1]) {
-        const x = findEnd[0];
-        const y = findEnd[1];
-        const pathDirection: Array<Array<number>> = [];
-        // Up
-        if (y > 0 && envBacktrack[x][y - 1] !== notAllowedPattern) {
-          const isUp = [x, y - 1];
-          if (env[x][y - 1] === notAllowedPattern) pathDirection.push(isUp);
-        }
-        //   Down
-        if (y < height - 1 && envBacktrack[x][y + 1] !== notAllowedPattern) {
-          const isDown = [x, y + 1];
-          if (env[x][y + 1] === notAllowedPattern) pathDirection.push(isDown);
-        }
-        //  Right
-        if (x < width - 1 && envBacktrack[x + 1][y] !== notAllowedPattern) {
-          const isRight = [x + 1, y];
-          if (env[x + 1][y] === notAllowedPattern) pathDirection.push(isRight);
-        }
+  }
 
-        //  Left
-        if (x > 0 && envBacktrack[x - 1][y] !== notAllowedPattern) {
-          const isLeft = [x - 1, y];
-          if (env[x - 1][y] === notAllowedPattern) pathDirection.push(isLeft);
-        }
-        pathDirection.sort((a, b) => {
-          const aDist = distanceBetweenTwoPoints(
-            a[0],
-            a[1],
-            snakeFood[0],
-            snakeFood[1]
-          );
-          const bDist = distanceBetweenTwoPoints(
-            b[0],
-            b[1],
-            snakeFood[0],
-            snakeFood[1]
-          );
-          return aDist - bDist;
-        });
-        if (pathDirection.length >= 1) {
-          const ind = pathDirection.length - 1;
-          findEnd = [...pathDirection[0]];
-          backtrackPath.push(pathDirection[0]);
-        } else {
-          break;
-        }
-      }
-      setSnakePath(backtrackPath);
+  function gameMovement() {
+    if (snakeFood) {
+      const snakeMovement = pathFinder();
+      moveSnakeBody(snakeMovement || 'D');
     }
-  }, [setSnakePathFind, snakeBody, snakeFood]);
-  // useFrameLoop(() => {
-  //   moveSnakeBody();
-  // });
+  }
+
+  useFrameLoop(() => {
+    gameMovement();
+  });
 
   return (
     <div className="mt-24 -z-10">
-      <div className="flex">
+      {/* <div className="flex">
         <button
           className="btn btn-primary"
           type="button"
-          onClick={moveSnakeBody}
+          onClick={gameMovement}
         >
           Move
         </button>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={() => changeMovement('U')}
-        >
-          ^
-        </button>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={() => changeMovement('D')}
-        >
-          v
-        </button>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={() => changeMovement('L')}
-        >
-          {'<'}
-        </button>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={() => changeMovement('R')}
-        >
-          {'>'}
-        </button>
-      </div>
+        <input
+          type="checkbox"
+          defaultChecked={createBarrier}
+          onClick={() => handleCheckChange()}
+          className="checkbox checkbox-secondary"
+        />
+      </div> */}
+      {isDead && <p className="text-xl font-bold text-error"> Game over</p>}
+      <p className="text-xl font-bold text-accent">
+        <strong>Score : </strong>
+        {score}
+      </p>
       <div className="absolute flex">
         {Array.apply(0, Array(width)).map((x, i) => {
           const colKey = `${i}_col_snake_env`;
@@ -328,6 +349,7 @@ function Home() {
                   );
                 }
                 return (
+                  // eslint-disable-next-line jsx-a11y/no-static-element-interactions
                   <div
                     className="absolute w-5 h-5 bg-base-300 shadow-sm rounded-sm "
                     style={{
@@ -335,6 +357,7 @@ function Home() {
                       top: `${j * 22 + 5}px`,
                     }}
                     key={rowKey}
+                    onMouseDown={() => addBarrier(i, j)}
                   />
                 );
               })}
