f0d7ca8e332f85774637036c05fb12508427c414
zalven-official <zalvendayao888@gmail.com>
2022-10-07 23:18:13 +0800

    deploying

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/snake-ai/',
+  base: '/Snake-AI/',
   plugins: [react()],
   test: {
     globals: true,
